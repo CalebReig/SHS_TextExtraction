0f6ee8136897fb8429fb64b9b294386c6dcfcba4
CalebReig <44947576+CalebReig@users.noreply.github.com>
2022-05-14 11:39:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ st.title('SHS Digitization Project')
 st.markdown("### Convert '.doc' Files to CSV")
 files = st.file_uploader("Upload Your File(s)", type=['.doc'], accept_multiple_files=True)
 if files:
-    combine_files(files)
+    df = combine_files(files)
+    st.markdown("### Sample Data")
+    st.write(df.head())
+    new_file = df.to_csv().encode('utf-8')
+    st.download_button('Download CSV', new_file, file_name='SHS.csv')
 
 st.markdown("### Contact""")
 st.markdown("For any issues or questions regarding using this application, contact Caleb Reigada at reigadacaleb@gmail.com")
