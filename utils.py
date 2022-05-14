f1a30a2334db5b0457f80b7e38b20787152ce3e3
CalebReig <44947576+CalebReig@users.noreply.github.com>
2022-05-14 11:39:08 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,7 +12,5 @@ def combine_files(files):
             df = extract_text('uploaded_file.doc')
         else:
             df = pd.concat(df, extract_text('uploaded_file.doc'), axis=0)
-    st.markdown("### Sample Data")
-    st.write(df.head())
-    new_file = df.to_csv().encode('utf-8')
-    st.download_button('Download CSV', new_file, file_name='SHS.csv')
+    return df
+
