f8adb63fa90032596c0304d5505df4710d291b5e
CalebReig <44947576+CalebReig@users.noreply.github.com>
2022-05-20 05:39:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if files:
     df = combine_files(files)
     st.markdown("### Sample Data")
     st.write(df.head())
-    new_file = df.to_csv()
-    st.download_button('Download CSV', new_file, file_name='SHS.csv', encoding='utf-8')
+    new_file = df.to_csv().encode('utf-8')
+    st.download_button('Download CSV', new_file, file_name='SHS.csv')
 
 st.markdown("### Contact""")
 st.markdown("For any issues or questions regarding using this application, contact Caleb Reigada at reigadacaleb@gmail.com")
